c170a241d9ac9f159568dac032166ebc6f1d4225
Andres Marquez <andresmr955@gmail.com>
2023-10-11 21:40:22 -0400

    Manipulacion DOM

--- MOKEPON/js/mokepon.js
+++ MOKEPON/js/mokepon.js
@@ -1,8 +1,34 @@
-function seleccionarMascotaJugador(){
-    return alert("seleccionaste la mascota " + botonMascotaJugador)
+function iniciarJuego(){
+    //El metodo getElementById nos sirve para llamar cualquier elemendo con el ID en especidifico
+    // Esta es la variable que creamos para seleccionar el boton mascota
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+ 
 }
 
-//El metodo nos sirve para llamar cualquier elemendo con el ID en especidifico
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 
+function seleccionarMascotaJugador(mascota){
+    // Creamos estas variables para que sea mas legible el codigo y no sea tan extenso dentro del condicional y se pueden usar los metodos en las variables
+    let inputHipodoge = document.getElementById('Hipodoge')
+    let inputCapipepo = document.getElementById('Capipepo')
+    let inputRatigueya = document.getElementById('Ratigueya')
+    let inputAndresillo = document.getElementById('Andresillo')
+    let mascotaSpanJugador = document.getElementById('mascota-jugador')
+
+    if(inputHipodoge.checked){
+        mascotaSpan.innerHTML = 'Hipodoge'
+    }else if(inputCapipepo.checked){
+        mascotaSpan.innerHTML = 'Capipepo'
+    }
+    else if(inputRatigueya.checked){
+        mascotaSpan.innerHTML = 'Ratigueya'
+    }else if(inputAndresillo.checked){
+        mascotaSpan.innerHTML = 'Andresillo'
+    }else{
+        alert('Debes seleccionar una mascota')
+    }
+}
+
+
+//Este metodo nos sirve para cargar toda la pagina
+window.addEventListener('load', iniciarJuego)
