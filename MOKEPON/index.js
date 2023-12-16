2c14d4a3a83d3fa4f4ea8f3a0c35448566a38498
Andres Marquez <andresmr955@gmail.com>
2023-12-15 21:17:12 -0500

    Servidor web con Express.js

--- MOKEPON/index.js
+++ MOKEPON/index.js
@@ -1,1 +1,11 @@
-console.log("Hola node de Andres")
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hola Andres")
+})
+
+app.listen(8080, () => {
+    console.log("Servidor funcionando")
+})
