eabaa376a3eb49a24d57d05a18241f08ea93b8a3
Andres Marquez <andresmr955@gmail.com>
2023-12-22 22:05:35 -0500

    Primer Dia 30diasJS

---
+++ 30DIASJS/diaUnoEjercicios.js
@@ -0,0 +1,30 @@
+/* Los  comentarios pueden hacer el
+código legible, fácil de usar e informativo */
+let nombrea = "Andres", //String
+ soyPapa = true, //Boolean
+ carro = undefined ,// undefined
+ volar = null // null
+
+console.log(typeof "Nombre = ", nombrea,  "Papa =>", soyPapa, "Tiene carro ", carro,  "Sabe volar ", volar)
+let gato,
+perro,
+perico, 
+leon
+
+let gatoa = "gato" ,
+perroa = "perro",
+pericoa = "perico", 
+leona = "leon"
+
+let nombre_
+let apellido_
+let estadoCivil_
+let pais_
+let edad_
+
+let nombre, apellido, estadoCivil, pais, edad
+
+let miEdad = 24
+let tuEdad = 25
+
+console.log("Tengo", miEdad, " y tu edad es ", tuEdad)
---
+++ 30DIASJS/holamundo.js
@@ -0,0 +1,13 @@
+console.log('¡Hola, Mundo! carpeta 2')
+// Declarando variables con valores numéricos
+let edad = 100 // edad en años
+const gravedad = 9.81 // gravedad terrestre en m/s2
+const puntoDeEbullición  = 100 // punto de ebullición del agua, temperatura en oC
+const PI = 3.14 // constante geométrica
+
+console.log(gravedad, puntoDeEbullición, PI)
+
+let nombre = 'Asabeneh', // nombre de una persona
+trabajo = 'profesor',
+viveEn = 'Finlandia';
+console.log(nombre, trabajo, viveEn);
