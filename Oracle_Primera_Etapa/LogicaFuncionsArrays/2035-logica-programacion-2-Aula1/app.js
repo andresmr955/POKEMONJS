5359112c04b85d037ab06b0888d9e7e0ace60ca8
Andres Marquez <andresmr955@gmail.com>
2024-01-25 17:13:04 -0500

    Logica programacion listas

--- Oracle_Primera_Etapa/LogicaFuncionsArrays/2035-logica-programacion-2-Aula1/app.js
+++ Oracle_Primera_Etapa/LogicaFuncionsArrays/2035-logica-programacion-2-Aula1/app.js
@@ -1,5 +1,75 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = "Juego del Numero secreto"
+let numeroRandom = 0;
+let intentos = 0;
+let numerosSorteados = [];
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = "Indica un numero del 1 al 10 "
+const asignarTextoElemento = (elemento, texto) => {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto
+    
+}
+
+
+const verificarIntento = () => {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(intentos)
+
+    if(numeroDeUsuario == numeroRandom){
+            asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${intentos === 1?'intento': 'Intentos'} el numero es ${numeroRandom}`);
+            document.getElementById('reiniciar').removeAttribute('disabled')
+        }else{ if(numeroDeUsuario > numeroRandom){
+            asignarTextoElemento('p', 'El numero secreto es menor');
+        }else{
+            asignarTextoElemento('p', 'El numero secreto es mayor');
+
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return ;
+}
+
+
+const limpiarCaja = () => {
+    document.querySelector('#valorUsuario').value = '';
+    
+}
+
+
+const consejosIniciales = () => {
+    asignarTextoElemento('p', "Indica un numero del 1 al 10");
+    asignarTextoElemento('h1', "Comenzando el juego Andres");
+     //generar el numero aleatorio 
+     numeroRandom = generarNumeroSecreto();
+     //inicializar intentos
+     intentos = 1;
+
+}
+
+const generarNumeroSecreto = () => {
+    let numeroGenerado = Math.floor(Math.random()*10)+1;
+
+    if(numerosSorteados.length == numeroMaximo){
+        asignarTextoElemento('p', 'Ya se sortearon todos los numeros posibles')
+    }else{        
+        if(numerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        }else{
+            numerosSorteados.push(numeroGenerado)
+            return numeroGenerado
+        }
+    }
+}
+
+const reiniciarJuego = () => {
+    //limpiar caja
+    limpiarCaja();
+    // Indicar mensaje de intervalo de numeros
+    consejosIniciales();
+   
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true')
+   
+    // Deshabilitar el boton de nuevo juego
+}
+
+consejosIniciales();
+
